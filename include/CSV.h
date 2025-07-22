d3c22e1c96cd92f6916bca5c392124a87f1edbe5
Sayver206 <santinogerevini8@gmail.com>
2025-07-21 23:50:54 -0300

    Create CSV.h

---
+++ include/CSV.h
@@ -0,0 +1,12 @@
+#ifndef CSV_H
+#define CSV_H
+
+class CSV {
+public:
+    void exportarSocios();
+    void exportarLibros();
+    void exportarPrestamos();
+    void exportarCuotas();
+};
+
+#endif // CSV_H
